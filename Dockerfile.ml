3ba6c364da920d81d6eac70e56c5405723d5f183
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2024-02-20 12:52:17 +0200

    Update package versions

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -2,7 +2,7 @@ ARG FULL_TAG=latest
 FROM --platform=$TARGETPLATFORM apluslms/grade-python:math-$FULL_TAG
 
 RUN pip_install \
-    networkx~=2.8.8 \
+    networkx==3.2.1 \
     pandas~=1.5.3 \
     scikit-learn \
  && :
